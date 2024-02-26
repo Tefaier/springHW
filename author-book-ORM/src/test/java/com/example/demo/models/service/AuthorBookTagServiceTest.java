7d1a2b6d8eac741cc6bf26a4a9b37f77b444f549
Tefaier <Tefaier@gmail.com>
2024-02-26 10:19:54 +0300

    adding of db context

--- author-book-ORM/src/test/java/com/example/demo/models/service/AuthorBookTagServiceTest.java
+++ author-book-ORM/src/test/java/com/example/demo/models/service/AuthorBookTagServiceTest.java
@@ -1,26 +1,33 @@
 package com.example.demo.models.service;
 
 import com.example.demo.models.DTO.*;
-import com.example.demo.models.entity.Book;
+import com.example.demo.models.DBSuite;
 import com.example.demo.models.entity.ChangeType;
-import com.example.demo.models.repository.AuthorRepository;
-import com.example.demo.models.repository.BookRepository;
-import com.example.demo.models.repository.TagRepository;
-import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest(classes = {
-    AuthorServiceImpl.class, BookServiceImpl.class, TagServiceImpl.class,
-    BookRepository.class, AuthorRepository.class, TagRepository.class
+@DataJpaTest
+@Transactional(propagation = Propagation.NOT_SUPPORTED)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import({
+    AuthorServiceImpl.class, BookServiceImpl.class, TagServiceImpl.class
 })
-class AuthorBookTagServiceTest {
+class AuthorBookTagServiceTest extends DBSuite {
   @Autowired
   private AuthorService authorService;
   @Autowired
